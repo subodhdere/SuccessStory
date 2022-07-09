629264d3f47f20924df57d085f64e3f3908969cd
Subodh <32792363+subodhdere@users.noreply.github.com>
2022-07-09 22:18:54 +0530

    Terraform

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+  access_key = "AKIA4HVH6VMMABDMVLGR"
+  secret_key = "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"
+}
+
 resource "aws_instance" "first_ec2_instance" {
   ami = "ami-08df646e18b182346"
   instance_type = "t2.micro"
   count = 1
   key_name = "ec2-key"
-  vpc_security_group_ids  = [aws_security_group.allow_ssh.id]
 }
