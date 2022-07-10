498875ec56523f99827d4d5e7b30fcf906b2b3e9
Subodh <32792363+subodhdere@users.noreply.github.com>
2022-07-10 10:23:29 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -10,3 +10,20 @@ resource "aws_instance" "first_ec2_instance" {
   count = 1
   key_name = "ec2-key"
 }
+
+resource "aws_eip" "lb" {
+  vpc      = true
+}
+
+output "eip" {
+  value = aws_eip.lb
+}
+
+resource "aws_s3_bucket" "mys3" {
+  bucket = "kplabs-attribute-demo-001"
+}
+
+output "mys3bucket" {
+  value = aws_s3_bucket.mys3
+}
+
