586ee2ae599b18fd70632827d8be50a6ade68c41
Subodh <32792363+subodhdere@users.noreply.github.com>
2022-07-10 19:12:39 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -7,8 +7,8 @@ provider "aws" {
 resource "aws_instance" "first_ec2_instance" {
   ami = "ami-08df646e18b182346"
   instance_type = "t2.micro"
-  count = 1
-  key_name = "ec2-key"
+#  count = 1
+#  key_name = "ec2-key"
 }
 
 resource "aws_eip" "lb" {
@@ -18,12 +18,3 @@ resource "aws_eip" "lb" {
 output "eip" {
   value = aws_eip.lb
 }
-
-resource "aws_s3_bucket" "mys3" {
-  bucket = "kplabs-attribute-demo-001"
-}
-
-output "mys3bucket" {
-  value = aws_s3_bucket.mys3
-}
-
