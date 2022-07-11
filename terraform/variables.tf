9908eb905e685c92ddd97566bfb6e1bd5533db5d
Subodh <32792363+subodhdere@users.noreply.github.com>
2022-07-11 12:18:08 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,15 @@ variable "instancetype" {
   type = string
   default = "t2.micro"
 }
+
+resource "aws_instance" "first_ec2_instance" {
+  ami = "ami-08df646e18b182346"
+  instance_type = var.my_instance_type
+}
+
+# terraform plan -var="instancetype=t2.small"
+
+# File : terraform.tfvars
+# instancetype="t2.small"
+
+# terraform plan -var-file="custom.tfvars"
