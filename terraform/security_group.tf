c99f75584372c5df4083cb394eb278b9c1034acf
Subodh <32792363+subodhdere@users.noreply.github.com>
2022-07-11 11:46:48 +0530

    Create security_group.tf

---
+++ terraform/security_group.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "my_sg" {
+  name        = "vpc-ssh-sd"
+  description = "VPC SSH"
+  ingress {
+    description = "Allow Port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Allow all ip and ports outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
