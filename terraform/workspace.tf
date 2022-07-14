609a0422afad0206c8f181e39107779bb5b28cf2
Subodh <32792363+subodhdere@users.noreply.github.com>
2022-07-14 15:52:14 +0530

    Update workspace.tf

--- terraform/workspace.tf
+++ terraform/workspace.tf
@@ -3,8 +3,8 @@ resource "aws_instance" "myec2" {
    instance_type = lookup(var.instance_type,terraform.workspace)
 }
 variable "instance_type" {
-  type = "map"
-default = {
+  type = map
+  default = {
     default = "t2.nano"
     dev     = "t2.micro"
     prd     = "t2.large"
