12c82c415c46b0b4e60c16f0ab5aa5ba8ec7d7d8
Subodh <32792363+subodhdere@users.noreply.github.com>
2022-07-14 14:11:25 +0530

    Update dynamic_block.tf

--- terraform/dynamic_block.tf
+++ terraform/dynamic_block.tf
@@ -42,7 +42,7 @@ resource "aws_security_group" "demo_sg" {
 variable "sg_ports" {
   type        = list(number)
   description = "list of ingress ports"
-  default     = [8200, 8201,8300, 9200, 9500]
+  default     = [8200, 8201, 8300, 9200, 9500]
 }
 
 resource "aws_security_group" "dynamicsg" {
@@ -56,17 +56,16 @@ resource "aws_security_group" "dynamicsg" {
       to_port     = egress.value
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
-            }
-
-  dynamic "ingress" {
-    for_each = var.sg_ports
-    iterator = port
-    content {
-      from_port   = port.value
-      to_port     = port.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-            }
+    }
   }
- }
+    dynamic "ingress" {
+      for_each = var.sg_ports
+      iterator = port
+      content {
+        from_port   = port.value
+        to_port     = port.value
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+    }
 }
