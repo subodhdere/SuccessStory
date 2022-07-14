035de8d39f53ed050db1ebcc0cb6be57cfebb325
Subodh <32792363+subodhdere@users.noreply.github.com>
2022-07-14 13:24:05 +0530

    Update remote_provisioner.tf

--- terraform/remote_provisioner.tf
+++ terraform/remote_provisioner.tf
@@ -61,7 +61,16 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  egress {
+
+   ingress {
+    description = "SSH into VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   egress {
     description = "Outbound Allowed"
     from_port   = 0
     to_port     = 65535
